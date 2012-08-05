8a489a5aa68cb9f84064cca98c31efe27ccaf46a
Ben Mays <benmays@Seans-MacBook-Air.local>
2012-08-04 21:08:16 -0400

    Fixed OAuth config vars.

--- app/etsy.py
+++ app/etsy.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, g, url_for, redirect, session, request
+from flask import Blueprint, g, url_for, redirect, session, request, flash
 from flaskext.oauth import OAuth
-import urllib
 
 from app import main
 
@@ -13,8 +12,11 @@ api = oauth.remote_app('etsy',
     access_token_url= main.config['ETSY_ACCESS_TOKEN_URL'],
     authorize_url= main.config['ETSY_AUTHORIZE_URL'],
     consumer_key= main.config['ETSY_CONSUMER_KEY'],
-    consumer_secret= main.config['ETSY_CONSUMER_SECRET']
+    consumer_secret= main.config['ETSY_CONSUMER_SECRET'],
+    request_token_params=main.config['ETSY_SCOPE']
 )
+
+
 
 ### routes ###
 @etsy.route('/')
@@ -25,14 +27,16 @@ def index():
 
     return "%s %s" % access_token
 
+### auth routes ###
 @etsy.route('/register')
 def register():
-    return api.authorize(callback=url_for('etsy.verify',
-        next=request.args.get('next') or request.referrer or None))
+    print url_for('etsy.verify')
+    return api.authorize(callback=url_for('etsy.verify', next=request.args.get('next') or request.referrer or None))
 
 @etsy.route('/verify')
 @api.authorized_handler
 def verify(resp):
+    print resp
     next_url = request.args.get('next') or url_for('etsy.index')
     if resp is None:
         flash(u'You denied the request to sign in.')
@@ -46,7 +50,85 @@ def verify(resp):
     flash(u'You were signed in!')
     return redirect(next_url)
 
-### utilities ###
+### api routes ###
+@etsy.route('/users/')
+@etsy.route('/users/<id>')
+def get_user(id=None):
+    if id is None:
+        id = "__SELF__"
+
+    resp = api.get('http://openapi.etsy.com/v2/users/'+id+'/')
+
+    if resp.status == 200:
+        data = resp.data
+    else:
+        data = None
+        
+    return str(data)
+
+@etsy.route('/users/<userid>/transactions/')
+def get_transactions(userid=None):
+    if userid is None:
+        userid = "__SELF__"
+
+    resp = api.get('http://openapi.etsy.com/v2/users/'+userid+'/transactions/')
+    print resp.data
+    if resp.status == 200:
+        data = resp.data
+    else:
+        data = None
+        
+    return str(data)
+
+@etsy.route('/users/<userid>/transactions/<transactionid>')
+def get_transaction(userid=None, transactionid=None):
+    if userid is None:
+        userid = "__SELF__"
+
+    if transactionid is None:
+        return get_transactions(userid)
+
+    resp = api.get('http://openapi.etsy.com/v2/users/'+userid+'/transactions/' + transactionid)
+
+    if resp.status == 200:
+        data = resp.data
+    else:
+        data = None
+        
+    return str(data)
+
+@etsy.route('/users/<userid>/shops/')
+def get_shops(userid=None):
+    if userid is None:
+        userid = "__SELF__"
+
+    resp = api.get('http://openapi.etsy.com/v2/users/'+userid+'/shops/')
+    print resp.data
+    if resp.status == 200:
+        data = resp.data
+    else:
+        data = None
+        
+    return str(data)
+
+@etsy.route('/users/<userid>/shops/<shopid>')
+def get_shop(userid=None, shopid=None):
+    if userid is None:
+        userid = "__SELF__"
+
+    if shopid is None:
+        return get_shops(userid)
+
+    resp = api.get('http://openapi.etsy.com/v2/users/'+userid+'/shops/' + shopid)
+
+    if resp.status == 200:
+        data = resp.data
+    else:
+        data = None
+        
+    return str(data)
+
+### utility routes ###
 @api.tokengetter
 def get_token():
     return session.get('etsy_token')
