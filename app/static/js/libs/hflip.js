2f510aed7011cdefffa3aa6248e8013f240c04a7
Ben Mays <benmays@Seans-MacBook-Air.local>
2012-08-05 11:29:07 -0400

    Pushing changes...

--- app/static/js/libs/hflip.js
+++ app/static/js/libs/hflip.js
@@ -1,25 +1,31 @@
 $(document).ready(function(){
-	var margin = $(".image1").width()/2;
-	var width = $(".image1").width();
-	var height = $(".image1").height();
-
-	$(".image2").stop().css({width:'0px',height:''+height+'px',marginLeft:''+margin+'px',opacity:'0.5'});
-
-	$("#reflection2").stop().css({width:'0px',height:''+height+'px',marginLeft:''+margin+'px'});
-
+var margin =$(".image1").width()/2;
+var width=$(".image1").width();
+var height=$(".image1").height();
+$(".image2").stop().css({width:'0px',height:''+height+'px',opacity:'0.5'});
+$("#reflection2").stop().css({width:'0px',height:''+height+'px',marginLeft:''+margin+'px'});
 	$(".image1").click(function(){
 		$(this).stop().animate({width:'0px',height:''+height+'px',marginLeft:''+margin+'px',opacity:'0.5'},{duration:250});
 		var self = this;
 		window.setTimeout(function() {
-			$(self).stop().animate({width:''+width+'px',height:''+height+'px',marginLeft:'0px',opacity:'1'},{duration:250});
+			//$(self).stop().animate({width:''+width+'px',height:''+height+'px',marginLeft:'0px',opacity:'1'},{duration:250});
+			console.log($(self).parent().find('.image2'))
+			$(self).parent().find('.image2').animate({width:''+width+'px',height:''+height+'px',opacity:'1'},{duration:250});
 		},500);
 	});
 	$(".image2").click(function(){
-		$(this).stop().animate({width:'0px',height:''+height+'px',marginLeft:''+margin+'px',opacity:'0.5'},{duration:250});
+		$(this).stop().animate({width:'0px',height:''+height+'px',opacity:'0.5'},{duration:250});
+
 		var self = this;
 		window.setTimeout(function() {
-			$(self).stop().animate({width:''+width+'px',height:''+height+'px',marginLeft:'0px',opacity:'1'},{duration:250});
+			//$(self).stop().animate({width:''+width+'px',height:''+height+'px',opacity:'1'},{duration:250});
+			$(self).parent().find('.image1').animate({width:''+width+'px',marginLeft: "0px", height:''+height+'px',opacity:'1'},{duration:250});
 		},500);
 	});
+
+
+	$(".grid-item").click(function(e){
+
+	})
 });
 
